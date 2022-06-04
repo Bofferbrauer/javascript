e8770999c484535ab1157af117073423dd0716de
Steve Weidig <75318450+Bofferbrauer@users.noreply.github.com>
2022-06-04 13:45:14 +0200

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,22 +1,42 @@
 let hello = "Want to play a game of rock-paper-scissors in the console?";
 alert(hello)
 
-function playRound(playerSelection, computerSelection) {
 
-      let player = prompt("Rock, Paper or Scissors? :");
-      playerSelection = player.toLowerCase();
-      console.log(playerSelection);
+let player = prompt("Rock, Paper or Scissors? :");
+playerSelection = player.toLowerCase();
+ console.log(playerSelection);
 
-      const choices = ["rock", "paper", "scissors"];
-      computerSelection = choices[Math.floor(Math.random() * choices.length)];
-      console.log(computerSelection);
+const choices = ["rock", "paper", "scissors"];
+computerSelection = choices[Math.floor(Math.random() * choices.length)];
+console.log(computerSelection);
 
       
-}
 
 function game() {
-      for (let i = 5; i > 0; i--) {
-            
+      for (let rounds = 5; rounds > 0; rounds--) {
+            if (computerSelection == playerSelection) {
+                  return "It's a Draw"
+            }
+
+            else if (computerselection = "rock" && (playerSelection = "paper")) {
+                  return "You won! Paper beats Rock."
+            }
+            else if (computerselection = "rock" && (playerSelection = "scissors")) {
+                  return "You lost! Rock beats Scissors."
+            }
+            else if (computerselection = "paper" && (playerSelection = "rock")) {
+                  return "You lost! Paper beats Rock."
+            }
+            else if (computerselection = "paper" && (playerSelection = "scissors")) {
+                  return "You won! Scissors beat Paper."
+            }
+            else if (computerselection = "scissors" && (playerSelection = "rock")) {
+                  return "You won! Rock beats Scissors."
+            }
+            else if (computerselection = "scissors" && (playerSelection = "paper")) {
+                  return "You lost! Scissors beat Paper."
+            }
+            else return "wrong input"
 
             
       }
